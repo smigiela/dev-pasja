eccd1a8adf5edb573470261088490730567fb21a
Dev-Pasja Daniel Śmigiela <daniel.smigiela@outlook.com>
2023-02-15 19:56:42 +0100

    +polish translations custom

---
+++ dev-pasja-app/lang/pl/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed'   => 'Błędny login lub hasło.',
+    'password' => 'Podane hasło jest nieprawidłowe.',
+    'throttle' => 'Za dużo nieudanych prób logowania. Proszę spróbować za :seconds sekund.',
+];
